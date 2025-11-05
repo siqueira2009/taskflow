0074a4b5d9387365b647e20778092cb02e079afd
Lucas Siqueira <lucas.s.siqueira.2009@gmail.com>
2025-11-04 21:07:25 -0300

    Adicionado cards responsivos (troca)

--- homepage/script.js
+++ homepage/script.js
@@ -41,3 +41,39 @@ alterarTema.addEventListener('click', () => {
         document.getElementById('temaEscuro').style.display = 'block'
     }
 })
+
+// Cards responsivos
+
+let cardsContainer = document.getElementById('cardsResp');
+let cardsResponsivos = document.querySelectorAll('.cardResp');
+let bolinhasContainer = document.getElementById('dotsResp');
+let bolinhas = bolinhasContainer.children;
+let cardAtual = 0;
+let cliques = 0;
+
+function trocarCard(card) {
+    cardsContainer.children[0].style.opacity = '0';
+
+    cardsResponsivos[card].classList.remove('focused');
+    cardsResponsivos[card].classList.add('notFocused');
+    cardsResponsivos[card].style.display = 'none';
+    bolinhas[card].classList.remove('posAtual')
+    if (card == 2) {
+        card = 0;
+    } else {
+        card++;
+    }
+    cardsResponsivos[card].style.display = 'flex'
+    cardsResponsivos[card].classList.add('focused');
+    cardsResponsivos[card].classList.remove('notFocused');
+    bolinhas[card].classList.add('posAtual')
+}
+
+cardsContainer.addEventListener('click', () => {
+    trocarCard(cardAtual);
+    if (cardAtual == 2) {
+        cardAtual = 0;
+    } else {
+        cardAtual++;
+    }
+})
